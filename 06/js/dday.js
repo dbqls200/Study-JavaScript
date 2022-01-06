f926580711cdf4416401e8080e6e6ddf9ebb5182
yubeen <dbqls200@gmail.com>
2022-01-06 18:57:45 +0900

    day 11

---
+++ 06/js/dday.js
@@ -0,0 +1,25 @@
+var now = new Date();
+var first = new Date('2019-10-22');
+
+var toNow = now.getTime();
+var toFirst = first.getTime();
+
+var passedTime = toNow - toFirst;
+var passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));
+
+function calcDate(days) {
+    var future = toFirst + days * (1000 * 60 * 60 * 24);
+    var someday = new Date(future);
+    var year = someday.getFullYear();
+    var month = someday.getMonth() + 1;
+    var date = someday.getDate();
+    
+    document.querySelector('#date' + days).innerHTML = year + '년 ' + month + '월 ' + date + '일';
+}
+
+document.querySelector('#accent').innerHTML = passedDay + '일';
+
+calcDate(100);
+calcDate(200);
+calcDate(365);
+calcDate(500);
